642ab2e03a38438b3a74fc28cf0795fabdbabb4c
riyadhossen74 <riyadhossen696969@gmail.com>
2025-11-09 23:04:52 +0600

    firebase sucure

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB4DbmvSwWBa5KTaHhsO-MQapHjlM8cXaA",
-  authDomain: "eco-track-79ea2.firebaseapp.com",
-  projectId: "eco-track-79ea2",
-  storageBucket: "eco-track-79ea2.firebasestorage.app",
-  messagingSenderId: "905894698397",
-  appId: "1:905894698397:web:f6a49c02be7b80063be374"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
-// Initialize Firebase
+// Optional: log to check if variables are loaded correctly
+console.log("Firebase Config:", firebaseConfig);
+
 export const app = initializeApp(firebaseConfig);
